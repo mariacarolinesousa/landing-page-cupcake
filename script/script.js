426027780ef852744820105943a142b8548b0444
Maria Caroline Sousa <carolinesousa@tutanota.com>
2025-11-18 13:54:22 -0300

    backend do modal

---
+++ script/script.js
@@ -0,0 +1,19 @@
+const btnPedido = document.getElementById("btn-modal");
+const modal = document.getElementById("modal");
+const closeModal = document.getElementById("fechar");
+
+//ao clicar no botão, abre o modal
+btnPedido.addEventListener("click", () => {
+    modal.style.display = "block";
+});
+
+closeModal.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+//se clicar no x, fecha o modal
+modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        modal.style.display = "none";
+    }
+});
